58fa849bc94e86375bc1791cba2aa8cf8e2322e3
inesr <inesrz333@gmail.com>
2026-03-12 17:48:29 +0100

    Corrección clase EdificioComercial

--- Ecocity/src/ecocity/EdificioComercial.java
+++ Ecocity/src/ecocity/EdificioComercial.java
@@ -1,29 +1,33 @@
 package ecocity;
+
 public class EdificioComercial extends Edificio {
 	private int bonusFelicidad;
 	private double ingresosMensuales;
 
 	public EdificioComercial(String nombre, int bonusFelicidad, double ingresosMensuales) {
-		super(nombre, 15_000+ ingresosMensuales * 10, 5.0);
+		super(nombre, 15000+ ingresosMensuales * 10, 5.0);
 		this.bonusFelicidad= bonusFelicidad;
 		this.ingresosMensuales= ingresosMensuales;
 	}
 
 	@Override
 	public void aplicarEfectoMensual() {
-		if (estaDestruido()) {
-			System.out.println("Está cerrado.", nombre);
+		if (this.getSalud()<= 0) {
+			System.out.println(nombre + " Cerrado por mal estado.");
 			return;
 		}
-		EstadoCiudad estado= EstadoCiudad.getInstance();
+
+		EstadoCiudad estado =EstadoCiudad.getInstance();
 		estado.agregarFelicidad(bonusFelicidad);
 		estado.agregarDinero(ingresosMensuales);
-		desgastar(2);
-		System.out.println("Felicidad, Ingresos%n", nombre, bonusFelicidad, ingresosMensuales);
+
+		// Bajamos salud usando el método de la clase padre
+		setSalud(getSalud()- 2);
+		System.out.printf("Generó de felicidad y de ingresos.", nombre, bonusFelicidad, ingresosMensuales);
 	}
 
 	@Override
 	public String toString() {
-		return super.toString() + String.format("Felicidad", bonusFelicidad);
+		return super.toString() + String.format("Bonus Felicidad:", bonusFelicidad);
 	}
 }
