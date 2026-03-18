1cbcf3d359507378d4a49c520ffc87549a88f384
Alba <alba.cantillo.o@gmail.com>
2026-03-18 17:54:03 +0100

    cambio main

--- src/principal/main.java
+++ src/principal/main.java
@@ -2,8 +2,6 @@ package principal;
 
 public class main {
  public static void main(String[] args) {
-	 System.out.println("¡Bienvenido a SimCity 2024!");
-	 System.out.println("Aquí podrás construir tu ciudad, gestionar recursos y enfrentar desafíos urbanos.");
-	 System.out.println("¡Comencemos a construir tu metrópolis ideal!");
+	 
  }
 }
